c39fb3558840c611fbcc9363621b84c216d2ce19
Angele Williams <angele.williams5@gmail.com>
2021-12-17 12:09:36 -0700

    Model developer object for Part III

---
+++ models/modelB.js
@@ -0,0 +1,13 @@
+//Part III: Modeling Using object - Developer
+
+var developer = {
+  softwareFocus: 'Frontend',
+  primaryLanguage: 'JavaScript',
+  yearsExperience: 0,
+  turingCohort: 2201,
+  currentStudent: true,
+  isExpert: false,
+  excitedAbout: ['starting Mod1', 'building community','learning new skills'],
+  studyPlans: ['code everyday', 'review notes', 'ask for help', 'use Google']
+}
+console.log(developer);
