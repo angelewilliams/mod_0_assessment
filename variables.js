012da2e5c111be20374e292985da44891fef2a03
Angele Williams <angele.williams5@gmail.com>
2021-12-17 11:46:54 -0700

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,11 @@
+//Techincal Assessment
+
+var movie = "Ratatouille";
+var isEntertaining = true;
+movie = "101 Dalmations";
+var num4 = 3452;
+var num6 = 748;
+var product = (num4 * num6);
+var favoriteFoods = ["potatoes", "ice cream", "green beans"];
+favoriteFoods.shift();
+console.log(favoriteFoods.length);
