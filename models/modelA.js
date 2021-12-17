9cb94aaead7573bb76f6e7cdf7a9f2ffb56fc120
Angele Williams <angele.williams5@gmail.com>
2021-12-17 11:59:17 -0700

    Model dinosaur in object for Part III

---
+++ models/modelA.js
@@ -0,0 +1,14 @@
+///Part III Modeling Using objects
+
+var dinosaur = {
+  genus: 'brontosaurus',
+  periodLived: 'Late Jurassic Period',
+  estimatedLifeSpanYrs: 100,
+  firstRecord: 1870,
+  herbivore: true,
+  longTail: true,
+  bodyCharacteristics: ['large','long-neck','whip like tail', 'weighed up to 15 tons'],
+  species: ['Brontosaurus excelsus', 'Brontosaurus parvus', 'Brontosaurus yahnahpin']
+}
+//Log object 'dinosaur' to console to check work
+console.log(dinosaur);
